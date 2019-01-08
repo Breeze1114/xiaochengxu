ce7617586e112e014b2871b525e3c939eef17d6f
Breeze <1052832729@qq.com>
2019-01-08 11:26:17 +0800

    搞好了循环出来的选择器，改变一个值，其他的值也跟着变得问题

--- miniprogram/pages/workDetail/workDetail.js
+++ miniprogram/pages/workDetail/workDetail.js
@@ -8,39 +8,43 @@ Page({
   data: {
     token: app.globalData.token,
     workId: app.globalData.work_id,
-    workResult:{},
-    workInfo:{},
-    date:{
-      value:'',
-      disabled:false
+    workResult: {},
+    workInfo: {},
+    date: {
+      value: '',
+      disabled: false
     },
-    list:[],
-    result:"请输入检查结果",
-    leaderList:[],
-    approver:"请选择审核人员"
+    list: [],
+    index: 0,//数组循环的下标
+    listIndex: 0,
+    safeList: [],//作为中间量的结果list
+    leaderList: [],
+    approver: "请选择审核人员"
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     var status = options.status;
     var work = JSON.parse(options.work);
     var jumpUrl = '';
     that.setData({
       workId: options.workId,
-      workInfo:work,
+      workInfo: work,
     });
-    if(status === '已提交' || status === '已完成'){
+    if (status === '已提交' || status === '已完成') {
       jumpUrl = 'http://10.1.40.150:3080/api/app/checkUser/work/' + that.data.workId + '/info';
-    }else{
+    } else {
       jumpUrl = 'http://10.1.40.150:3080/api/app/checkUser/work/' + that.data.workId + '/info';
     }
     wx.request({
       url: jumpUrl,
       data: '',
-      header: { 'Authorization': 'Bearer ' + app.globalData.token },
+      header: {
+        'Authorization': 'Bearer ' + app.globalData.token
+      },
       method: 'GET',
       dataType: 'json',
       responseType: 'text',
@@ -52,9 +56,9 @@ Page({
         var checkDate = that.data.workResult.check_date;
         if (checkDate != null) {
           that.setData({
-            date:{
-              value : checkDate,
-              disabled : true
+            date: {
+              value: checkDate,
+              disabled: true
             },
           })
         }
@@ -65,50 +69,69 @@ Page({
     wx.request({
       url: 'http://10.1.40.150:3080/api/app/org/getAuditUserList',
       data: '',
-      header: { 'Authorization': 'Bearer ' + app.globalData.token },
+      header: {
+        'Authorization': 'Bearer ' + app.globalData.token
+      },
       method: 'GET',
       dataType: 'json',
       responseType: 'text',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           leaderList: res.data.data
         })
       },
-      fail: function (res) { },
-      complete: function (res) { },
+      fail: function(res) {},
+      complete: function(res) {},
     });
     wx.request({
       url: 'http://10.1.40.150:3080/api/app/resultNameList',
       data: '',
-      header: { 'Authorization': 'Bearer ' + app.globalData.token },
+      header: {
+        'Authorization': 'Bearer ' + app.globalData.token
+      },
       method: 'GET',
       dataType: 'json',
       responseType: 'text',
-      success: function (res) {
+      success: function(res) {
         that.setData({
           list: res.data.data
-        })
+        });
+        var arr = that.data.safeList;
+        var list = that.data.list;
+        //给中间量的list初始化值
+        for (var i = 0; i < list.length; i++) {
+          arr.push("请输入检查结果");
+        }
+        that.setData({
+          safeList: arr
+        });
       },
-      fail: function (res) { },
-      complete: function (res) { },
+      fail: function(res) {},
+      complete: function(res) {},
     });
   },
 
-  showDate: function(e){
+  showDate: function(e) {
     var that = this;
     that.setData({
       date: e.detail.value
     })
   },
 
-  pickerValChange: function(e){
-    var that =this;
+  pickerValChange: function(e) {
+    var that = this;
+    var arr = that.data.safeList;
+    var list = that.data.list;
+    var value = e.detail.value;
+    var index = e.currentTarget.dataset.index;
+    //点击了哪个选择器，就修改哪个下标的值，然后就可以展示出去了
+    arr[index] = list[value];
     that.setData({
-      result: that.data.list[e.detail.value]
+      safeList: arr
     })
   },
 
-  auditPickerChange: function(e){
+  auditPickerChange: function(e) {
     var that = this;
     that.setData({
       approver: that.data.leaderList[e.detail.value].user_name
